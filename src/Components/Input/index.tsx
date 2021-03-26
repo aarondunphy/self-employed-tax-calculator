bd4c343e126ed6ebc42445e48a2ec48faedbcb62
Aaron <aaronjohndunphy@gmail.com>
2021-03-26 13:56:19 +0000

    Tailwind installed

--- src/Components/Input/index.tsx
+++ src/Components/Input/index.tsx
@@ -7,7 +7,14 @@ type Props = {
 };
 
 const Input = ({ type = "text", name, placeholder }: Props) => {
-  return <input type={type} name={name} placeholder={placeholder} />;
+  return (
+    <input
+      type={type}
+      name={name}
+      placeholder={placeholder}
+      className="focus:ring-indigo-500 focus:border-indigo-500 block w-full py-2 px-5 sm:text-sm border-2 border-gray-300 rounded-md"
+    />
+  );
 };
 
 export default Input;
